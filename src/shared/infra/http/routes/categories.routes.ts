ca644c3973cec74b0d3d0329ccf6662b74d59e9d
alicesilva <alice.silva@ccc.ufcg.edu.br>
2021-04-21 17:17:28 -0300

    testes de integraçao

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,7 @@ import multer from "multer"; // yarn add multer e yarn add @types/multer
 import { CreateCategoryController } from "../../../../modules/cars/usesCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "../../../../modules/cars/usesCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "../../../../modules/cars/usesCases/listCategories/ListCategoriesController";
+import { ensureAuthentication } from "../middlewares/ensureAuthenticate";
 
 const categoriesRoutes = Router();
 const upload = multer({
@@ -14,7 +15,11 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+  "/",
+  ensureAuthentication,
+  createCategoryController.handle
+);
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
--- src/shared/infra/http/routes/specifications.route.ts
+++ src/shared/infra/http/routes/specifications.route.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "../../../../modules/cars/usesCases/createSpecification/CreateSpecificationController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthentication } from "../middlewares/ensureAuthenticate";
 
 const specificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthentication);
-specificationsRoutes.post("/", createSpecificationController.handle);
+// specificationsRoutes.use(ensureAuthentication);
+specificationsRoutes.post(
+  "/",
+  ensureAuthentication,
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 export { specificationsRoutes };
